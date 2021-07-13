96e1b285bc629d2e1f893bb5fc3d79e925741c1d
Gustavo Pires <gustavo.pires@gmail.com>
2021-07-13 16:50:54 +0100

    initial commit

---
+++ src/test/java/DummyTestCase.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class DummyTestCase {
+
+	@Test
+	public void dummyTest() {
+		assertEquals(4,4);
+	}
+}
